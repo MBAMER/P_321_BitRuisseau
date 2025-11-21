41c5272865d927e93c0746e24199d533d09dbd10
MBAMER <163824331+MBAMER@users.noreply.github.com>
2025-11-21 10:30:42 +0100

    Feat(Design): J'ai créer l'application et design le frontend

---
+++ Code/P_Bit_Ruisseau/P_Bit_Ruisseau/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace P_Bit_Ruisseau
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            panel3 = new Panel();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = SystemColors.ActiveCaptionText;
+            label1.Font = new Font("Segoe UI", 18F);
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(401, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(168, 32);
+            label1.TabIndex = 0;
+            label1.Text = "P_Bit_Ruisseau";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ButtonFace;
+            panel1.Location = new Point(54, 169);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(249, 322);
+            panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ButtonFace;
+            panel2.Location = new Point(357, 169);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(249, 322);
+            panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = SystemColors.ButtonFace;
+            panel3.Location = new Point(656, 169);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(249, 322);
+            panel3.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(314, 71);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 3;
+            label2.Text = "label2";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15F);
+            label3.ForeColor = SystemColors.ButtonFace;
+            label3.Location = new Point(54, 138);
+            label3.Name = "label3";
+            label3.Size = new Size(57, 28);
+            label3.TabIndex = 4;
+            label3.Text = "Local";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15F);
+            label4.ForeColor = SystemColors.ButtonFace;
+            label4.Location = new Point(357, 138);
+            label4.Name = "label4";
+            label4.Size = new Size(128, 28);
+            label4.TabIndex = 5;
+            label4.Text = "Médiathèque";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15F);
+            label5.ForeColor = SystemColors.ButtonFace;
+            label5.Location = new Point(656, 138);
+            label5.Name = "label5";
+            label5.Size = new Size(223, 28);
+            label5.TabIndex = 6;
+            label5.Text = "Nom de la médiathèque";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.Location = new Point(54, 71);
+            button1.Name = "button1";
+            button1.Size = new Size(177, 40);
+            button1.TabIndex = 7;
+            button1.Text = "Choisir un dossier";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(959, 542);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Panel panel2;
+        private Panel panel3;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+    }
+}
