f1338f462c42273e26d9e1a0d5689cade1b6b417
MBAMER <163824331+MBAMER@users.noreply.github.com>
2025-12-05 09:58:49 +0100

    Feat(Form1.cs)Rajout de la méthode ChoixDossier

--- Code/P_Bit_Ruisseau/P_Bit_Ruisseau/Form1.cs
+++ Code/P_Bit_Ruisseau/P_Bit_Ruisseau/Form1.cs
@@ -7,10 +7,22 @@ namespace P_Bit_Ruisseau
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ChoixDossier(object sender, EventArgs e)
         {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Choisissez un dossier";
+                dialog.UseDescriptionForTitle = true; 
 
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedFolder = dialog.SelectedPath;
+
+                    MessageBox.Show("Dossier sélectionné : " + selectedFolder);
+                }
+            }
         }
+
 
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
